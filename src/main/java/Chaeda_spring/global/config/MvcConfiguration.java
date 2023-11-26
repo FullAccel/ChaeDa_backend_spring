be06af7b783ed15efeb3b891fe05c42f7ca0d292
sejoon00 <parksejoon313@gmail.com>
2023-11-26 23:36:34 +0900

    [Fix] Cors 설정 변경

--- src/main/java/Chaeda_spring/global/config/MvcConfiguration.java
+++ src/main/java/Chaeda_spring/global/config/MvcConfiguration.java
@@ -13,7 +13,8 @@ public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
