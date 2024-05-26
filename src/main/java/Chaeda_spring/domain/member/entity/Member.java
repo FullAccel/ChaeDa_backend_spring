b24d00b28c9c7941a93643898349e82b2f0f9171
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-05-26 15:14:52 +0900

    로그인 회원가입 filter 안타게 수정

--- src/main/java/Chaeda_spring/domain/member/entity/Member.java
+++ src/main/java/Chaeda_spring/domain/member/entity/Member.java
@@ -23,7 +23,7 @@ public abstract class Member extends BaseTimeEntity {
     @JoinColumn(name = "image_id")
     Image image;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String loginId;
