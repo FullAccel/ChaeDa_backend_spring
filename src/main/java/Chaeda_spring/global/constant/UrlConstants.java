ad6350dae525f1c891fbe64195b0f680bcb88bec
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-05-27 21:24:41 +0900

    Fix: fastapi port 변경

--- src/main/java/Chaeda_spring/global/constant/UrlConstants.java
+++ src/main/java/Chaeda_spring/global/constant/UrlConstants.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum UrlConstants {
-    FASTAPI_SERVER_URL("http://114.70.23.79:8000"),
+    FASTAPI_SERVER_URL("http://114.70.23.79:80"),
     ;
 
     private String value;
