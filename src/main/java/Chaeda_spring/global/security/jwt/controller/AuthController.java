49ec0468d491f46c033606e7f70d0cefc147fe05
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-06-10 14:16:13 +0900

    Fix: jwt 재발급 테스트

--- src/main/java/Chaeda_spring/global/security/jwt/controller/AuthController.java
+++ src/main/java/Chaeda_spring/global/security/jwt/controller/AuthController.java
@@ -1,13 +1,11 @@
 package Chaeda_spring.global.security.jwt.controller;
 
 
-import Chaeda_spring.global.security.jwt.dto.TokenDto;
 import Chaeda_spring.global.security.jwt.service.JwtTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +19,7 @@ public class AuthController {
     @GetMapping("/token/reissue")
     @Operation(summary = "AT RT 재발급")
     public ResponseEntity<Void> reIssue(
-            @RequestBody TokenDto tokenDto
+//            @RequestBody TokenDto tokenDto
     ) {
 //        return ResponseEntity.ok(jwtTokenService.recreateTokenDto(tokenDto.refreshToken()));
         return ResponseEntity.ok().body(null);
